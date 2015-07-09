98c254823baa5b97ac0613eca3e495ba69255d28
Gaëtan FEREZ <gaetan@ferez.fr>
2015-07-09 16:07:21 +0200

    Show cert shell command with certid in arg if necessary to display cert detail

--- lib/ManPKI/Shell/Manage/ShCert.py
+++ lib/ManPKI/Shell/Manage/ShCert.py
@@ -17,10 +17,19 @@ class ShCert(ShShell):
     def do_uri(self, line):
         Config().config.set("ocsp", "uri", line)
 
-    def show_cert(self):
+    def show_cert(self, certid=None):
         list = []
-        for cert in SSL.get_all_certificates():
-            list.append((cert['id'], SSL.get_x509_name(cert['cert'].get_subject())))
-        Render.print_table(('ID', 'Subject'), list)
+        if certid:
+            i=0
+            for cert in SSL.get_all_certificates():
+                if certid == cert['id']:
+                    i=1
+                    SSL.display_cert(cert['cert'])
+            if i == 0:
+                print "*** Certificate not found"
+        else:
+            for cert in SSL.get_all_certificates():
+                list.append((cert['id'], SSL.get_x509_name(cert['cert'].get_subject())))
+            Render.print_table(('ID', 'Subject'), list)
 
 
