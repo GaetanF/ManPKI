1005a286db855db7393ce0385b02a0b65985cf1f
Gaëtan FEREZ <gaetan@ferez.fr>
2015-07-10 14:49:16 +0200

    Create generate_crl program

---
+++ bin/launch-cron-generate-crl.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=$PYTHONPATH:/Users/ferezgaetan/PycharmProjects/manpki/lib/ManPKI/ python ../lib/ManPKI/Cron/generate_crl.py
---
+++ lib/ManPKI/Cron/generate_crl.py
@@ -0,0 +1,32 @@
+__author__ = 'ferezgaetan'
+
+import os.path
+import OpenSSL
+import ConfigParser
+import Secret
+
+config = ConfigParser.ConfigParser()
+config.read(Secret.config_file)
+
+crl_file = config.get("default", "certdir") + "/public/ca/crl.pem"
+ca_file = config.get("default", "certdir") + "/public/ca/ca.crt"
+ca_private = config.get("default", "certdir") + "/private/ca.privkey"
+
+if os.path.exists(ca_file) and os.path.exists(ca_private) and config.getint("crl", "validity") and config.get("crl", "digest"):
+    ca_str = open(ca_file, "r").read()
+    ca_private_str = open(ca_private, "r").read()
+    ca_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, ca_str)
+    private_key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, ca_private_str)
+
+    crl = OpenSSL.crypto.CRL()
+    if os.path.exists(crl_file):
+        crl_str = open(crl_file, "r").read()
+        crl = OpenSSL.crypto.load_crl(OpenSSL.crypto.FILETYPE_PEM, crl_str)
+
+    crl_str = crl.export(ca_cert, private_key, type=OpenSSL.crypto.FILETYPE_PEM, days=config.getint("crl", "validity"))
+    f = open(crl_file, "w")
+    f.write(crl_str)
+    f.close()
+
+
+
