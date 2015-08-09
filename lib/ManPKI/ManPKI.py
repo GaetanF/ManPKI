8f2ec081a6dd0201891e905d248eda6d3840ddd9
Gaëtan FEREZ <gaetan@ferez.fr>
2015-08-09 14:37:38 +0200

    Remove EventManager from ManPKI master class

--- lib/ManPKI/ManPKI.py
+++ lib/ManPKI/ManPKI.py
@@ -1,5 +1,4 @@
 from ShShell import ShShell
-from Tools import EventManager
 
 
 class ManPKI:
@@ -8,11 +7,6 @@ class ManPKI:
 
     def __init__(self):
         """ Initialisation """
-        EventManager.addEvent(new_cert=[])
-        EventManager.addEvent(update_cert=[])
-        EventManager.addEvent(delete_cert=[])
-        EventManager.addEvent(new_ca=[])
-        EventManager.addEvent(update_ca=[])
 
     def shell(self):
         ShShell().cmdloop()
