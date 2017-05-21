98be0a889064a68d14b9549e387f14768f609de8
Gaëtan Ferez <gaetan@ferez.fr>
2017-05-21 09:11:07 +0200

    Remove api/locale

--- manpki/api/locale.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-locale_route = Blueprint('locales', __name__, url_prefix='/locales')
-
-
-@locale_route.route("/<locale>")
-def get_locales(locale):
-    locales = []
-    return locales, 200
